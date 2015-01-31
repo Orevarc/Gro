857a035d3ec3bcff9b644885f2986de4d4cf553a
Concept8 <connorhaslip@gmail.com>
2015-01-31 18:12:34 -0500

    Create Tables.sql Edits
    Added FoodCategory table. Changed varchars to nvarchars. Changed some
    column names.

--- SQL/Create Tables.sql
+++ SQL/Create Tables.sql
@@ -8,19 +8,17 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 CREATE TABLE [dbo].[User](
-	userID int NOT NULL PRIMARY KEY,
-	username varchar(20) NOT NULL,
-	pass varchar(20) NOT NULL,
-	email varchar(30) NOT NULL,
-	token varchar(20) NOT NULL,
+	userID int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	username nvarchar(50) NOT NULL,
+	pass nvarchar(512) NOT NULL,
+	email nvarchar(50) NOT NULL,
 	createdAt Date NOT NULL,
 	updatedAt Date NOT NULL
 )
 
 CREATE TABLE [dbo].[OwnedFoodItem](
-	ownershipID int NOT NULL PRIMARY KEY,
+	ownershipID int IDENTITY (1,1) NOT NULL PRIMARY KEY,
 	userID int NOT NULL,
 	foodItemID int NOT NULL,
 	dateBought Date NOT NULL,
@@ -28,26 +26,37 @@ CREATE TABLE [dbo].[OwnedFoodItem](
 	used Bit
 );
 
+/*foodCategoryID will help us derive 2 nvarchar values from the FoodCategory Table, factualCategory and generalCategory*/
+/*generalCategories are used to group factualCategories*/
+
 CREATE TABLE [dbo].[FoodItem](
-	foodItemID int NOT NULL PRIMARY KEY,
-	upcCode varchar(15),
-	itemName varchar(60) NOT NULL,
-	brand varchar(50) NOT NULL,
-	size varchar(25),
-	foodCategory varchar(30),
+	foodItemID int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	upcCode nvarchar(13) NOT NULL,
+	itemName nvarchar(60) NOT NULL,
+	brand nvarchar(50),
+	size nvarchar(25),
+	foodCategoryID int,
 	expectedExpiryDate int,
-	imageURL varchar(50)
+	imageURL varchar(70)
 );
 
 CREATE TABLE [dbo].[FavoritedRecipe](
-	favoriteRecipeID int NOT NULL PRIMARY KEY,
+	favoriteRecipeID int IDENTITY (1,1) NOT NULL PRIMARY KEY,
 	recipeID int NOT NULL,
 	userID int NOT NULL
 )
 
 CREATE TABLE [dbo].[Recipe](
-	recipeID int NOT NULL PRIMARY KEY,
-	recipeName varchar(20),
-	ingredients varchar(20),
-	amounts varchar(20)
+	recipeID int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	recipeName nvarchar(20),
+	ingredients nvarchar(20),
+	amounts nvarchar(20)
 )
+
+CREATE TABLE [dbo].[FoodCategory](
+	foodCategoryID int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	factualCategory nvarchar(50),
+	generalCategory nvarchar(50),
+	expiryTime int
+)
+
