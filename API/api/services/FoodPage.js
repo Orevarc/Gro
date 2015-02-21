581c077739cb94c3e98cd48f5f451004773c1b88
Michael Cravero <mjcravero@gmail.com>
2015-02-21 13:33:09 -0500

    Adding found items to a user's owned list

--- API/api/services/FoodPage.js
+++ API/api/services/FoodPage.js
@@ -2,16 +2,39 @@ var Promise = require('bluebird'),
     promisify = Promise.promisify;
 
 lookupUPC = function(options) {
-    var $FoodItem = API.Model(FoodItem);
+    var user_id = options.user_id;
     var upc = options.upc;
-    console.log('IN UPC');
-
-    return FoodItem.findOne({
-        upcCode: upc
-    }).then(function(fooditem) {
-        console.log('----Item');
+    var upcString = '' + upc
+    var query2 = "SELECT FoodItem.foodItemID, FoodItem.upcCode, FoodItem.itemName, FoodCategory.factualCategory, FoodCategory.generalCategory, FoodCategory.expiryTime FROM FoodItem INNER JOIN FoodCategory ON FoodItem.foodCategoryID = FoodCategory.foodCategoryID WHERE CAST(upcCode as numeric(13,0)) = " + upcString;
+    var queryAsync = Promise.promisify(FoodItem.query);
+    return queryAsync(query2).then(function(fooditem) {
+        //var item = JSON.parse(fooditem);
+        var date = new Date();
+        var expiry = new Date();
+        var foodID = fooditem[0].foodItemID;
+        console.log('----FoodItem1' + fooditem[0].foodItemID);
         console.log(fooditem);
-        return fooditem;
+
+        if (fooditem[0].expiryTime != null) {
+            expiry.setDate(expiry.getDate() + fooditem[0].expiryTime);
+        } else {
+            console.log('NULL EXPIRY');
+            expiry = null;
+        }
+        OwnedFoodItem.create({
+            user_id: user_id,
+            foodItemID: fooditem[0].foodItemID,
+            dateBought: date,
+            expiryDate: expiry,
+            used: 0
+        }).then(function(ownedfooditem) {
+            console.log('----FoodItem2');
+            console.log(fooditem);
+            console.log('----OWNEDFOODItem----------');
+            console.log(ownedfooditem);
+            return fooditem[0];
+        });
+
     });
 };
 
@@ -30,6 +53,8 @@ module.exports = {
 
     postListItems: function(data, context) {
         console.log(JSON.parse(data.items));
+        console.log('USER');
+        console.log(context.identity.id);
         var items = JSON.parse(data.items);
         var index = [];
         var masterList;
@@ -37,7 +62,8 @@ module.exports = {
             var upc = items[i]["upc"];
             console.log(upc);
             var test = lookupUPC({
-                upc: upc
+                upc: upc,
+                user_id: context.identity.id
             });
             console.log("TESTING");
             console.log(test);
